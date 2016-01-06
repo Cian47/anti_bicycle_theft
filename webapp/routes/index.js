bb8d459fcda18ec2f0dc3a930e00ae880b125c17
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2016-01-06 14:39:33 +0100

    Hopefully fixed the incorrect position number inside the badges. Note: Beware of js async behavior.

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var util = require('util');
 var monk = require('monk');
 var db = monk('localhost:27017/bikeapp');
 var users = db.get('users');
@@ -12,17 +11,16 @@ router.get('/', function(req,res){
 		var bikes = user.bikes;
 		//Create an array holding the amount of logged locations per bike so they can be shown in a badge
 		var count = 1;
-		for(bike of bikes){
+		bikes.forEach(function(bike){
 			positions.find({user_id:user._id, bike_name:bike.nickname},{}, function(e,docs){
-				//this relies on for(.. of..) to interate over array in order (a[0],a[1],...). Not sure if this is always the case
-				bikes[count-1].numberOfLogs = docs.length;
+				bikes[bikes.indexOf(bike)].numberOfLogs = docs.length;
 				if(count==bikes.length){
 					res.render('index', {username: username, bikes:bikes});
 				}
 				else
 					count+=1;
 			});
-		}
+		});
 	});
 	
 });
