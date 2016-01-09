6cda491e40078cf68ca838e0f9b2abd7d65a73bb
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-01-09 18:08:18 +0100

    python module, which will send and get data

---
+++ nesC/python/listen.py
@@ -0,0 +1,7 @@
+import subprocess
+import time
+p = subprocess.Popen(["java net.tinyos.tools.Listen -comm serial@/dev/ttyUSB1:iris &"], stdout=subprocess.PIPE, shell=True)
+
+while True:
+	pkt=p.stdout.readline().strip(" \n\r\t")  #reads until \n
+	print(pkt)
