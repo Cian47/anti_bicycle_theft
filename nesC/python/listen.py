219270a36cab2ea0f7ed7353b471a4785237be31
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-01-11 16:15:50 +0100

    listening to serialforwarder now

--- nesC/python/listen.py
+++ nesC/python/listen.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
-p = subprocess.Popen(["java net.tinyos.tools.Listen -comm serial@/dev/ttyUSB1:iris &"], stdout=subprocess.PIPE, shell=True)
+#p = subprocess.Popen(["java net.tinyos.tools.Listen -comm serial@/dev/ttyUSB1:iris &"], stdout=subprocess.PIPE, shell=True)
+p = subprocess.Popen(["java net.tinyos.tools.Listen &"], stdout=subprocess.PIPE, shell=True)
 
 while True:
 	pkt=p.stdout.readline().strip(" \n\r\t")  #reads until \n
