ea43330c72cf2553986b3d8aea7bae072dde178e
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-01-13 16:49:42 +0100

    right timestamp

--- python-api/env/bikeDB/listen.py
+++ python-api/env/bikeDB/listen.py
@@ -1,13 +1,14 @@
 import subprocess
 import time
 import bikeDB
+import datetime
 #p = subprocess.Popen(["java net.tinyos.tools.Listen -comm serial@/dev/ttyUSB1:iris &"], stdout=subprocess.PIPE, shell=True)
 p = subprocess.Popen(["java net.tinyos.tools.Listen &"], stdout=subprocess.PIPE, shell=True)
 
 def unsecret(secret):
     return (secret-9)/7
 
-bikeDB = new BikeDB()
+bDB = bikeDB.BikeDB()
 
 COORDS_PER_PACKET=2
 LENGTH_OF_PACKET=(2+4+4+4)*2 #in nibbles
@@ -15,6 +16,7 @@ while True:
 	pkt=p.stdout.readline().strip(" \n\r\t").lower()  #reads until \n
 	#00 00 08 00 1A 22 22 71 00 01 00 05 00 1A 24 EE 00 BF FF AA FF BB 00 00 00 00 03 12 AF BC 00 00 00 00 00 97 CD 2F 00 00 00 00
 	pkt=pkt.replace(" ","")
+	print(pkt)
 	if (pkt[30:32]=="ee"): #dissemination ID
 	    nodeid=pkt[32:36]
 	    print "sent by:",unsecret(int(nodeid,16)),"(%s)"%nodeid
@@ -22,13 +24,16 @@ while True:
 	    print "runtime:",runtime
 	    offset=44
 	    for i in range(0,COORDS_PER_PACKET):
-	        time=int(pkt[offset+i*8:offset+i*8+8],16)/1000.0
+	        t=int(pkt[offset+i*8:offset+i*8+8],16)/1000.0
 	        lat=int(pkt[offset+i*8+COORDS_PER_PACKET*8:offset+i*8+COORDS_PER_PACKET*8+8],16)/1000000.0
 	        lon=int(pkt[offset+i*8+COORDS_PER_PACKET*8+COORDS_PER_PACKET*8:offset+i*8+COORDS_PER_PACKET*8+COORDS_PER_PACKET*8+8],16)/1000000.0
 	        
-	        print "time:",time
+	        print "time:",t
 	        print "lat:",lat
 	        print "lon:",lon
-	        bikeDB.insertPosition(nodeid,lat,lon,time.time()-(runtime-time))
+	        timestamp=datetime.fromtimestamp(time.time()-int(runtime-t).isoformat()
+	        print "timestamp:",timestamp
+	        print "insert:","%d"%int(nodeid,16),lat,lon,timestamp)
+	        if (lat<99 and lon<99 and lat!=0.0 and lon!=0.0):
+	            bDB.insertPosition("%d"%int(nodeid,16),str(lat),str(lon),timestamp)
 	        #lat = pkt[
-	print(pkt)
